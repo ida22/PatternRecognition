5cc84279f7dddf4250de3e3c4a0e62b884341c91
ida22 <czyrek.ida@gmail.com>
2019-01-20 22:30:30 +0100

    wyszyszczony hopfield

--- Recognize/Models/Hopfield.cs
+++ Recognize/Models/Hopfield.cs
@@ -7,13 +7,9 @@ namespace Recognize.Models
     {
         #region Declarations
 
-        private Matrix _weights;
-        public double[,] _lepszeWeights;
-        private Matrix _output;
-        private int[] _lepszeOutput;
-        private Matrix _offsetNeuronWeights;
-        private long _iterations = 0;
-        private int _neuronCount = 0;
+        public double[,] weights;
+        private int[] output;
+        private int neuronCount = 0;
         private const double THRESHOLD_CONSTANT = 0.0;
 
 		public bool trained = false;
@@ -22,73 +18,18 @@ namespace Recognize.Models
 
         #region Constructor
 
-        /// <summary>
-        /// Method that accepts a pattern(s) from which to train. Returns the current Weight matrix. 
-        /// Each column of the input matrix represents a pattern to be learnt. The number of neurons
-        /// generated is determined by the number of rows in the matrix.
-        /// </summary>
-        /// <param name="trainingData">Matrix containing the training pattern(s).</param>
-        public Matrix Train(Matrix trainingData)
-        {
-            //see if this is the first piece of training data
-            if (_neuronCount == 0)
-            {
-                //get the size of the network, i.e. the number of neurons
-                _neuronCount = trainingData.RowCount;
-
-                //create the weights matrix
-                _weights = new Matrix(_neuronCount, _neuronCount);
-
-                //create the output vector
-                _output = new Matrix(_neuronCount, 1);
-
-                //create the offset Neuron weights (used to determine thresholds)
-                _offsetNeuronWeights = new Matrix(_neuronCount, 1);
-
-            }
-
-            //set data to bipolar representation
-            trainingData = trainingData.ToBiPolar();
-
-            //transpose and multiply by self
-            Matrix trainingDataT = Matrix.Transpose(trainingData);
-            Matrix result = Matrix.Multiply(trainingData, trainingDataT);
-
-            //neurons do not have connections to themselves so subtract a unit matrix (with the diagonal set
-            //to the number of paterns)
-            Matrix unit = new Matrix(trainingData.RowCount, 1.0);
-            result = Matrix.Subtract(result, unit);
-
-            //now we have the calculated pattern so add to the weights
-            _weights = Matrix.Add(_weights, result);
-
-            //now calculate the thresholds using a neuron with its output fixed at 1.
-            //first create a vector of all 1's that is the same size as the number of patterns passed in
-            //this allows us to calculate the offSetNeuron weights as we did above.            
-            Matrix offsetNeuron = new Matrix(trainingData.ColumnCount, 1, 1.0);
-
-            //multiply offsetNeuron with input pattern(s) and add to _offsetNeuronWeights.
-            result = Matrix.Multiply(trainingData, offsetNeuron);
-            _offsetNeuronWeights = Matrix.Add(_offsetNeuronWeights, result);
-
-#if(TRACE)
-            Console.WriteLine(string.Format("Weights: {0}", _weights.ToString("f0")));
-#endif
-            return _weights;
-        }
-
         public void TrainByPseudoInverse(int[,] testData)
         {
             testData = testData.Multiply(2).Subtract(1); // macierz -1 i 1
 
-            int rowCount = testData.Rows(); //liczba wzorców
-            _neuronCount = testData.Columns(); //liczba neuronów we wzorcu
+            int patternCount = testData.Rows(); //liczba wzorców
+            neuronCount = testData.Columns(); //liczba neuronów we wzorcu
 
-            double[,] W = new double[_neuronCount, _neuronCount]; //inicjalizacja macierzy wag 64x64
+            double[,] W = new double[neuronCount, neuronCount]; //inicjalizacja macierzy wag 64x64
 
-            for(int row = 0; row < rowCount; row++)
+            for(int row = 0; row < patternCount; row++)
             {
-                double[,] x = testData.Get(row, row + 1, 0, _neuronCount).Transpose().Convert(i => (double)i);
+                double[,] x = testData.Get(row, row + 1, 0, neuronCount).Transpose().Convert(i => (double)i);
 
                 var x1 = W.Dot(x).Subtract(x);
                 var x1t = x1.Transpose();
@@ -97,41 +38,29 @@ namespace Recognize.Models
                 double mianownik = x.TransposeAndDot(x).Subtract(x.Transpose().Dot(W).Dot(x))[0,0];
 
                 W = W.Add(licznik.Divide(mianownik));
-
-                //for (int i = 0; i < 64; i++)
-                //{
-                //    W[i, i] = 0;
-                //}
             }
 
-            //for (int i = 0; i < 64; i++)
-            //{
-            //    W[i, i] = 0;
-            //}
-
-            _lepszeWeights = W;
-            var suma = W.Sum();
-            _weights = new Matrix(W);
+            weights = W;
 
 			trained = true;
 		}
 
-        public int[] LepszyTest(int[] testData)
+        public int[] Test(int[] testData)
         {
             testData = testData.Multiply(2).Subtract(1);
 
-            long maxIterations = Convert.ToInt64(Math.Pow(_neuronCount, 2));
+            long maxIterations = Convert.ToInt64(Math.Pow(neuronCount, 2));
             //bool isStable = false;
 
             //create an ordered array and then shuffle it
-            int[] updateOrder = new int[_neuronCount];
-            for (int index = 0; index < _neuronCount; index++)
+            int[] updateOrder = new int[neuronCount];
+            for (int index = 0; index < neuronCount; index++)
             {
                 updateOrder[index] = index;
             }
             updateOrder = Shuffle(updateOrder);
 
-            _lepszeOutput = testData;
+            output = testData;
 
             int updatedCount = 0;
             int iterationCount = 0;
@@ -140,185 +69,22 @@ namespace Recognize.Models
             {
                 updatedCount = 0;
 
-                for(int i=0; i<_neuronCount; i++)
+                for(int i=0; i<neuronCount; i++)
                 {
                     int neuronId = updateOrder[i];
-                    var Vin = _lepszeWeights.GetColumn(neuronId).Dot(_lepszeOutput);
+                    var Vin = weights.GetColumn(neuronId).Dot(output);
                     var V = (Vin >= 0 ? 1 : -1);
-                    if(_lepszeOutput[neuronId] != V)
+                    if(output[neuronId] != V)
                     {
                         updatedCount++;
-                        _lepszeOutput[neuronId] = V;
+                        output[neuronId] = V;
                     }
                 }
 
                 iterationCount++;
             } while (updatedCount != 0 && iterationCount < maxIterations);
 
-            return _lepszeOutput;
-        }
-
-        /// <summary>
-        /// Method that accepts a pattern for the network to test. Patterns are represented in columns.
-        /// Returns an binary output matrix that matches the training matrix if the testdata
-        /// was matched to the training data.
-        /// </summary>
-        public Matrix Test(Matrix testData)
-        {
-            //_neuronCount = 64; // TRZEBA TU COS Z TYM ZROBIC!!!!
-            //_weights = new Matrix(_neuronCount, _neuronCount);
-            //_output = new Matrix(_neuronCount, 1);
-            //_offsetNeuronWeights = new Matrix(_neuronCount, 1);
-
-            if (_neuronCount <= 0)
-                throw new NetworkException(Resources.ErrorMessages.NetworkNotTrained);
-            if (!testData.IsColumnVector)
-                throw new ArgumentException(Resources.ErrorMessages.ArgumentMatrixisNotColumnVector);
-            if (testData.RowCount != _neuronCount)
-                throw new ArgumentException(Resources.ErrorMessages.ArgumentBadSize);
-
-            //set the maximum number of iterations
-            long maxIterations = Convert.ToInt64(System.Math.Pow((double)_neuronCount, 2));
-            bool isStable = false;
-
-            //create an ordered array and then shuffle it
-            int[] updateOrder = new int[_neuronCount];
-            for (int index = 0; index < _neuronCount; index++)
-            {
-                updateOrder[index] = index;
-            }
-
-            updateOrder = Shuffle(updateOrder);
-
-            //set the output to the initial state
-            _output = testData;
-            _iterations = 0;
-
-            //loop through the maximum possible times or until network is stable
-            while (_iterations < maxIterations && isStable == false)
-            {
-
-                //assume stable
-                isStable = true;
-
-                //make the input the same as the output
-                Matrix input = _output;
-
-                //loop through each neuron, calculating the input and determining the output
-                for (int currentNeuron = 0; currentNeuron < _neuronCount; currentNeuron++)
-                {
-
-                    //get the next neuron to update
-                    int neuronToUpdate = updateOrder[currentNeuron];
-
-                    //get the weights for the neuron
-                    Matrix neuronWeights = _weights.GetRow(neuronToUpdate);
-
-                    //==================================================================================================
-                    // This code was originally used and was based entirely on Picton's description of the 
-                    // process (2000, p75). It was later refactored to use the Matrix.DotProduct() method.
-                    //==================================================================================================
-                    //calculate wieght of the current neuron
-                    //double neuronWeight = 0;
-                    //for (int inputLine = 0; inputLine < input.RowCount; inputLine++)
-                    //{
-                    //    //dont do if the input is from the current neuron as neuron outputs
-                    //    //do not connect to themselves
-                    //    if (inputLine != currentNeuron)
-                    //    {
-                    //        //multiply by associated input element so that only the inputLine set to 1 adds to the total
-                    //        neuronWeight += (input.GetElement(inputLine, 0) * neuronWeights.GetElement(0, inputLine));
-                    //    }
-                    //}
-                    //==================================================================================================
-
-                    //==================================================================================================
-                    // Refactored alternative approach to code above, this works because each neurons connection to
-                    // itself was 'zeroed out' during the training phase.
-                    //==================================================================================================
-                    double neuronWeight = Matrix.DotProduct(neuronWeights, input);
-                    //==================================================================================================
-
-                    //get the threshold for the current neuron
-                    //the threshold is = the negated weight therefore multiply by -1
-                    double threshold = _offsetNeuronWeights.GetElement(neuronToUpdate, 0) * -1.0;
-
-                    //output 1 if the weight is above the threshold and 0 if the weight is below the threshold
-                    //if the weight equals the threshold, make no change to whats already there.
-                    //the THRESHOLD_CONSTANT is a value used in experiments and would typically be set to 0
-                    if (neuronWeight > threshold + THRESHOLD_CONSTANT)
-                    {
-                        //set the output to 1 if it is not already
-                        if (_output.GetElement(neuronToUpdate, 0) != 1)
-                        {
-                            _output.SetElement(neuronToUpdate, 0, 1);
-                            isStable = false;
-                        }
-                    }
-                    else if (neuronWeight < threshold)
-                    {
-                        //set the output to 0 if it is not already
-                        if (_output.GetElement(neuronToUpdate, 0) != 0)
-                        {
-                            _output.SetElement(neuronToUpdate, 0, 0);
-                            isStable = false;
-                        }
-                    }
-
-                    //if state has changed then exit this loop and move to next state
-                    if (!isStable)
-                    {
-                        break;
-                    }
-                    //else loop to next neuron
-                }
-
-                //keep a count of how many times we have been through the process
-                _iterations++;
-
-            }
-
-#if(TRACE)
-            Console.WriteLine(string.Format("Output: {0}, Iterations: {1}", _output.ToString("f0"), _iterations.ToString()));
-#endif
-            return _output;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the number of neurons in the network.
-        /// This property is updated after the Train() method has been invoked.
-        /// </summary>
-        public int NeuronCount
-        {
-            get { return _neuronCount; }
-        }
-        /// <summary>
-        /// Returns the current weight values.
-        /// This property is updated after the Train() method has been invoked.
-        /// </summary>
-        public Matrix Weights
-        {
-            get { return _weights; }
-        }
-        /// <summary>
-        /// Returns the current output values.
-        /// This property is updated after the Test() method has been invoked.
-        /// </summary>
-        public Matrix Output
-        {
-            get { return _output; }
-        }
-        /// <summary>
-        /// Returns the number of iterations needed to identify the test data. 
-        /// This property is updated after the Test() method has been invoked.
-        /// </summary>
-        public long Iterations
-        {
-            get { return _iterations; }
+            return output;
         }
 
         #endregion
